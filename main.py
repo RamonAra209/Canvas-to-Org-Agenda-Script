06e92744e32d651c1e5b19c36080af1974eb39fe
Ramon Arambula <r_arambula@u.pacifc.edu>
2022-09-04 11:47:13 -0700

    Added todo writting to org

--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class CanvasConnection:
         self.user = self.canvas.get_current_user()
         self.favorited_classes = self._get_favorited_classes()
         self.assignments_json = self._get_json()
+        self.added_lines = {}
 
     def _get_json(self) -> dict:
         with open(JSON_PATH, 'r') as f:
@@ -48,18 +49,24 @@ class CanvasConnection:
         """ This is only called once per semester
             It builds out initial json without courses
         """
+        with open(JSON_PATH) as f:
+            data: dict = json.load(f)
+
         courses = {}
         for course in self.favorited_classes:
             course_name:str = course.name
+            if course_name in data.keys():
+                continue
             courses[course_name] = {}
+            print("Built dict for ", course_name)
 
-        with open(JSON_PATH) as f:
-            data = json.load(f)
 
         data.update(courses)
 
         with open(JSON_PATH,'w') as f:
             json.dump(data, f, indent=4, sort_keys=True)
+
+        self.assignments_json = data
 
 
     def build_assignment_json(self):
@@ -75,45 +82,70 @@ class CanvasConnection:
 
                 is_graded = assignment_data["assignment"].get('grading_type', None)
 
+                # Filterng out reading assignments, unless its in course whitelist
                 if not course_name in COURSE_WHITELIST:
                     if not is_graded or is_graded == "not_graded":
                         continue
 
+
+                print(f"Building assignment '{name}' for course '{course_name}'")
                 unlock_date = assignment_data["assignment"].get('unlock_at', None)
-                due_date = assignment_data["assignment"].get('created_at', None)
+                due_date = assignment_data["assignment"].get('due_at', None)
                 link = assignment_data["assignment"].get('html_url', None)
 
                 assignment_info = {
-                    "unlock_date": unlock_date,
-                    "due_date": due_date,
+                    "unlock_date": convert_time(unlock_date),
+                    "due_date": convert_time(due_date),
                     "link": link,
                 }
                 self.assignments_json[course_name][name] = assignment_info
+                course_prefix = ''.join(course_name.split('-')[0:2])
+                self.added_lines[f"{course_prefix}: {name}"] = assignment_info
 
     def build_json(self):
         self.build_course_json()
         self.build_assignment_json()
         self._write_to_json()
 
+    def write_to_org(self):
+        with open(ORG_PATH, 'a') as f:
+            for assignment in self.added_lines.keys():
+                unlock_date = self.added_lines[assignment]["unlock_date"]
+                due_date = self.added_lines[assignment]["due_date"]
+                link = self.added_lines[assignment]["link"]
+
+                print("Writing org entry for ", assignment)
+                f.write(f"* TODO {assignment}\n")
+
+                # SCHEDULED: <2022-09-05 Mon 18:00> DEADLINE: <2022-09-09 Fri>
+                time_due = ""
+                if unlock_date:
+                    time_due += f"SCHEDULED: <{unlock_date}> "
+
+                time_due += f"DEADLINE: <{due_date}>"
+                f.write(f"{time_due}\n")
+                f.write(f"[[{link}][Link to assignment]]\n")
+
     def run(self):
         self.build_json()
+        print()
+        self.write_to_org()
 
-def convert_times(time_:str) -> str:
+def convert_time(time_) -> str:
     """ Converts from YYYY-MM-DDTHH:mm:ss.fZ -> YYYY-MM-DD DAY"""
+    if not time_:
+        return time_
+
     days = {0:"Mon", 1:"Tue", 2:"Wed", 3:"Thu", 4:"Fri", 5:"Sat", 6:"Sun"}
     important_part = time_.split("T")[0]
     format_ = "%Y-%m-%d"
     dt_obj = datetime.datetime.strptime(important_part, format_)
+    dt_obj -= datetime.timedelta(days=1)
     dt_str = ''.join(str(dt_obj).split(" ")[0])
     return f"{dt_str} {days[dt_obj.weekday()]}"
-    
+
+
 
 if __name__ == "__main__":
-    # driver = CanvasConnection(url=API_URL, token=API_KEY)
-    # driver.run()
-
-    res = convert_times(time_="2022-08-16T00:02:48Z")
-    print(res)
-
-    # some_todo = driver._get_todos(driver.favorited_classes[0])[0]
-    # pprint(some_todo.__dict__)
+    driver = CanvasConnection(url=API_URL, token=API_KEY)
+    driver.run()
